37d87e834ce80c124b556b51b76191b50937b9d7
ahmed-jobayer <jobayer320721@.com>
2024-04-30 20:24:19 +0600

    got data according to id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.port || 5000;
@@ -32,6 +32,15 @@ async function run() {
     app.get("/touristSpots", async (req, res) => {
       const cursor = touristSpotCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // view details page
+
+    app.get("/touristSpots/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristSpotCollection.findOne(query);
       res.send(result);
     });
 
